98e09bf2c1a3a87acecb593a01ab81a3dff74027
Boj99 <bojancvijan@hotmail.com>
2014-04-21 15:29:52 +0100

    Update Chrom_Freq_Parse.pl

--- Chrom_Freq_Parse.pl
+++ Chrom_Freq_Parse.pl
@@ -22,13 +22,13 @@ my @letter = $line =~ /\((\w{1})\)/g;
 
 use List::MoreUtils qw(each_array);
 
-# prepare the INSERT statement once
+# prepare the INSERT statement
 my $sth_insert = $dbh->prepare('INSERT INTO codon SET codon=?, codon_freq=?, one_letter_id=?')
-  or die $dbh->errstr;
+	or die $dbh->errstr;
 
 # create the array iterator
 my $ea = each_array(@codon, @freq, @letter);
 # iterate over all three arrays step by step
 while ( my ($val_a, $val_b, $val_c) = $ea->() ) {
-  $sth_insert->execute($val_a, $val_b, $val_c) or die $dbh->errstr;
+	$sth_insert->execute($val_a, $val_b, $val_c) or die $dbh->errstr;
 }
